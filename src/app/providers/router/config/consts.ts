2bc7ae08e6333fe58893b5ac3c1c2be7dc1bc222
Сальман Химишев <salman.khimishev01@gmail.com>
2025-12-15 13:09:30 +0300

    refactor(routing): use relative paths for nested routes

--- src/app/providers/router/config/consts.ts
+++ src/app/providers/router/config/consts.ts
@@ -1,8 +1,8 @@
 import { AppRoutes } from "./types"
 
 export const RoutePath: Record<AppRoutes, string> = {
-    [AppRoutes.MAIN]: '/main',
-    [AppRoutes.ABOUT]: '/about',
+    [AppRoutes.MAIN]: 'main',
+    [AppRoutes.ABOUT]: 'about',
 }
 
 export const NOT_FOUND_ROUTE = '*'
