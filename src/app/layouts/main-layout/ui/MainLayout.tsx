8da104a7dc2756e537c662bd0614c4a6dce0438e
Сальман Химишев <salman.khimishev01@gmail.com>
2025-12-16 22:20:38 +0300

    feat(*): integrate shadcn/ui

--- src/app/layouts/main-layout/ui/MainLayout.tsx
+++ src/app/layouts/main-layout/ui/MainLayout.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button"
 import { RoutePath } from "app/providers/router/config/consts"
 import { AppRoutes } from "app/providers/router/config/types"
 import { Link, Outlet } from "react-router-dom"
@@ -8,6 +9,7 @@ export const MainLayout = () => {
             <div className="mt-2 flex gap-2">
                 <Link to={RoutePath[AppRoutes.MAIN]}>Main page</Link>
                 <Link to={RoutePath[AppRoutes.ABOUT]}>About page</Link>
+                <Button>Click me</Button>
             </div>
             <Outlet />
         </div>
