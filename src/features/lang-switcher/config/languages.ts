9d345b8151001fe4298c45f5826584aaf987ab12
Сальман Химишев <salman.khimishev01@gmail.com>
2025-12-22 23:21:29 +0300

    feat(lang-switcher): add language switcher component to navbar

---
+++ src/features/lang-switcher/config/languages.ts
@@ -0,0 +1,32 @@
+export const languages = [
+    {
+        title: 'Русский',
+        locale: 'ru',
+        icon: '🇷🇺'
+    },
+    {
+        title: 'English',
+        locale: 'en',
+        icon: '🇬🇧'
+    },
+    {
+        title: 'Français',
+        locale: 'fr',
+        icon: '🇫🇷'
+    },
+    {
+        title: 'Español',
+        locale: 'es',
+        icon: '🇪🇸'
+    },
+    {
+        title: 'Deutsch',
+        locale: 'de',
+        icon: '🇩🇪'
+    },
+    {
+        title: 'Italiano',
+        locale: 'it',
+        icon: '🇮🇹'
+    }
+]
---
+++ src/features/lang-switcher/ui/LangSwitcher.tsx
@@ -0,0 +1,31 @@
+import { Globe, Check } from 'lucide-react'
+import { languages } from "../config/languages"
+import { useTranslation } from "react-i18next"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from 'shared/ui/dropdown-menu';
+
+export const LangSwitcher = () => {
+    const { i18n } = useTranslation();
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Globe />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                {languages.map(({ title, locale, icon }) => (
+                    <DropdownMenuItem
+                        className="flex gap-1 items-center"
+                        onClick={() => {
+                            i18n.changeLanguage(locale)
+                        }}
+                        key={locale}
+                    >
+                        <span>{icon}</span>
+                        <span>{title}</span>
+                        <span>{i18n.language === locale && <Check width={16} />}</span>
+                    </DropdownMenuItem>
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
